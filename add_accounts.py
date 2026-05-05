794fa9d5d5cff9ca7359136d0954e27558179f27
Anashe-Masomeke <masomekeanashe4@gmail.com>
2026-05-05 15:00:34 +0200

    Create add_accounts.py

---
+++ add_accounts.py
@@ -0,0 +1,5 @@
+from models import add_account
+
+add_account('0781440589', 'LOAN', 'LN-YOUR-001', 1500.00, '2026-12-31', 18.0, 150.0)
+add_account('0781440589', 'LIFE', 'LF-YOUR-001', 300.00, '2026-07-15', 0.0, 30.0)
+print('Done!')
